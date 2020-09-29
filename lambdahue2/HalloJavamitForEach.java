cd79f6864639a3b1515249dc212fb0041dc7c2a9
unknown <jonathanthalbauer@gmail.com>
2020-09-29 10:53:13 +0200

    Second commit

--- lambdahue2/HalloJavamitForEach.java
+++ lambdahue2/HalloJavamitForEach.java
@@ -5,10 +5,24 @@
  */
 package lambdahue2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Jonny
  */
 public class HalloJavamitForEach {
-    
+
+    public static void main(String[] args) {
+        String s = "";
+        List<String> list = new ArrayList<>();
+        list.add("Erste");
+        list.add("Zweite");
+        list.add("Dritte");
+
+        for (String l : list) {
+            System.out.println(l);
+        }
+    }
 }
--- lambdahue2/LambdaHue2.java
+++ lambdahue2/LambdaHue2.java
@@ -11,9 +11,7 @@ package lambdahue2;
  */
 public class LambdaHue2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
